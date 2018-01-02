b01a0d56a572986595dad5c6d3fe8634a402b199
yilmazanil <30016445+yilmazanil@users.noreply.github.com>
2018-01-02 23:51:01 +0300

    Initial AngularJs Commit

---
+++ AngularJsFundamentals/app/js/controllers/EventController.js
@@ -0,0 +1,16 @@
+'use strict'
+
+eventsApp.controller('EventController', function ($scope) {
+    $scope.event = {
+        name: 'Angular Boot Camp',
+        date: '1/1/2017',
+        time: '10:30 am',
+        location : {
+            address: 'Google Headquarters',
+            city: 'Mountain View',
+            province:'CA'
+        },
+        imageUrl:'/img/angularjs-logo.png'
+    };
+}
+);
